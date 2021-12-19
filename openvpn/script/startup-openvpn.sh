871a11cb97ce467a0453e5c0e39cb40b5913b74b
NineWoranop <NineWoranop@users.noreply.github.com>
2021-12-19 19:55:29 +0700

    Test daemon on openvpn

--- openvpn/script/startup-openvpn.sh
+++ openvpn/script/startup-openvpn.sh
@@ -4,12 +4,6 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
     chmod 600 /dev/net/tun
 fi
-if [ $# -eq 0 ]
-  then
-    /usr/sbin/openvpn --config /etc/openvpn/server/server.conf
-  else
-    /usr/sbin/openvpn --config /etc/openvpn/server/server.conf & $1
-fi
-
-
+/usr/sbin/openvpn --config /etc/openvpn/server/server.conf
+sleep infinity
 echo "exited shell script $0"
